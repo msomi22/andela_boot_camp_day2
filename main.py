d7506bebb71967eca2cd0f45d6a2c2c60134aa55
msomi22 <mwendapeter72@gmail.com>
2016-10-31 17:26:40 +0300

    more robust main.pay,remove employee.pyc

--- main.py
+++ main.py
@@ -4,33 +4,36 @@
 from employee import Employee 
 from student import Student 
 class Demo(Employee, Student):
+
 	def __init__(self):
-		pass 
+		super(Demo, self).__init__()  
+    #Save student/employee into RDMBS 
+	def put(self,obj):
+		if isinstance(obj,Student): 
+			print 'student' , obj 
+		else:
+			print 'employee' , obj 
+		 
 		
-		
 
-if __name__ == '__main__':
-	#Employee object
-	emp = Employee('100','IT','Developer','G','56,000')	
-	#user setters 
-	emp.setName('Peter Mwenda')
-	emp.setGender('Male')
-	emp.setDob('31-05-1990')
-	emp.setCounty('Tharaka Nithi')
-	print emp 
-	
 
-    #Student Object 
-	student = Student('100','Form 2 N') 
-	#user setters 
-	student.setName('Naomi Kawira')
-	student.setGender('Female')
-	student.setDob('16-07-1990')
-	student.setCounty('Meru') 
-	print student 
-	
+student = Student('100','Form 2 N') 
+demo = Demo()
 
-	 		 
+student.setName('Naomi Kawira')
+student.setGender('Female')
+student.setDob('16-07-1990')
+student.setCounty('Meru') 
+#demo.put(student) 
+
+emp = Employee('100','IT','Developer','G','56,000')	
+emp.setName('Peter Mwenda')
+emp.setGender('Male')
+emp.setDob('31-05-1990')
+emp.setCounty('Tharaka Nithi')
+# call put method 
+demo.put(emp)
+
 
 
 
